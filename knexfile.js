e1fbcab7afcc9d9d194b038ca35035fcc0a53f71
ndurbin <nick.durbin@gmail.com>
2019-12-30 07:48:35 -0500

    I added the pool and moved files inside dev object

--- knexfile.js
+++ knexfile.js
@@ -2,16 +2,22 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/recipes.db3'
+    }, 
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foriegn_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
+
   },
-  useNullAsDefault: true,
-  migrations: {
-    directory: './data/migrations'
-  },
-  seeds: {
-    directory: './data/seeds'
-  }
 
 };
