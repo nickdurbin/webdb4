1a7bd076ceebb5c873f5a0e01f66b2c9d56e5b77
ndurbin <nick.durbin@gmail.com>
2019-12-31 03:09:59 -0500

    Adjusted the seed data.

--- data/seeds/001_recipes.js
+++ data/seeds/001_recipes.js
@@ -3,8 +3,8 @@ exports.seed = function(knex) {
     .truncate()
     .then(function () {
       return knex('recipes').insert([
-        {id: 1, recipe_name: 'Ramen', ingredient_id: 1, quantity: 3, quantity_type: 'oz', instructions: 'Put in the microwave.'},
-        {id: 2, recipe_name: 'Orange', ingredient_id: 2, quantity: 1, quantity_type: 'item', instructions: 'Peel and eat.'},
+        {id: 1, recipe_name: 'Ramen', shopping_cart_id: 1, instructions: 'Put in the microwave.'},
+        {id: 2, recipe_name: 'Orange', shopping_cart_id: 2, instructions: 'Peel and eat.'},
       ]);
     });
 };
