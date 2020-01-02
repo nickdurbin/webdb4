70906700e5418d35e7e919fba4a7986a0cae3d94
ndurbin <nick.durbin@gmail.com>
2020-01-02 07:44:37 -0500

    Changed the order of the pool object in the knex config file.

--- knexfile.js
+++ knexfile.js
@@ -6,18 +6,16 @@ module.exports = {
     connection: {
       filename: './data/recipes.db3'
     }, 
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foriegn_keys = ON', done);
-      },
-    },
     migrations: {
       directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
-    }
-
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foriegn_keys = ON', done);
+      },
+    },
   },
-
 };
