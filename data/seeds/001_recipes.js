a012f5270fb899f3d947010d1e4222ec746ccec8
ndurbin <nick.durbin@gmail.com>
2020-01-02 07:22:31 -0500

    Updated the seed structure and data for recipes.

--- data/seeds/001_recipes.js
+++ data/seeds/001_recipes.js
@@ -3,8 +3,8 @@ exports.seed = function(knex) {
     .truncate()
     .then(function () {
       return knex('recipes').insert([
-        {id: 1, recipe_name: 'Ramen', shopping_cart_id: 1, instructions: 'Put in the microwave.'},
-        {id: 2, recipe_name: 'Orange', shopping_cart_id: 2, instructions: 'Peel and eat.'},
+        {id: 1, recipe_name: 'Ramen', quantity: 3, quantity_type: 'oz', instructions: 'Put in the microwave.'},
+        {id: 2, recipe_name: 'Orange', quantity: 1, quantity_type: 'Orange', instructions: 'Peel and eat.'},
       ]);
     });
 };
