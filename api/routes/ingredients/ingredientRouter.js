139afda10bee39b420597a885b29e100ba5ff8e0
ndurbin <nick.durbin@gmail.com>
2020-01-02 07:20:40 -0500

    Added some initial routes.

--- api/routes/ingredients/ingredientRouter.js
+++ api/routes/ingredients/ingredientRouter.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const Ingredients = require('../../../data/models/ingredient-model')
 
 router.get("/", (req, res, next) => {
   try {
